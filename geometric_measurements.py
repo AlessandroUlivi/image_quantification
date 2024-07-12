c75af614eff1cfa593fa4c9542e4d593e251f997
Alessandro Ulivi <ale.ulivi@gmail.com>
2024-07-12 12:08:25 +0200

    initialize get_mask_area in geometric_measurements.py

--- geometric_measurements.py
+++ geometric_measurements.py
@@ -1,9 +1,44 @@
 import numpy as np
 from scipy.spatial import ConvexHull
+from skimage.measure import label, regionprops
 
 
-def get_mask_area():
-    return
+def get_mask_area(input_array, roi_mas_k=None, binarization_threshold=0):
+    """
+    Returns the area of the region of interest in a binary mask both in terms of number of pixels of the mask and in terms of number of pixels scaled by pixel-area.
+
+    Inputs:
+    - input_array. ndarray. Binary array. Pixels of interest are assumed to be the pixels whose value is higher than binarization_threshold (default 0).
+    - roi_mas_k. Optional. ndarray of the same size of input_array. Binary mask. If provided, restricts the analysis to a region of interest. The region of interest is assumed to
+    the positive pixels in roi_mas_k.
+    - binarization_threshold. Int or float. Default 0. Defines the highpass threshold to distinguish pixels of interest from background in input_array. Pixels whose value
+    is >binarization_threshold are considered pixels of interest. The rest of the pixels are considered background.
+
+    Outputs: tuple.
+    - position-0. The area of the region of interest in input_array as total number of its pixels (the pixels of interest).
+    - position-1. The area of the region of interest in input_array as number of pixels scaled by pixel-area. Refer to
+    https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.regionprops for further documentation.
+    """
+    #Copy input_array and binarize it in the 0 and 1 range
+    input_array_01 = np.where(input_array>binarization_threshold, 1, 0)
+    
+    #If roi_mas_k is provided, copy it and use it to set to 0 pixels in input_array corresponded by background values in roi_mas_k
+    if hasattr(roi_mas_k, "__len__"):
+        roi_mas_k_copy = roi_mas_k.copy()
+        array_to_process = np.where(roi_mas_k_copy>0, input_array_01, 0)
+    else:
+        array_to_process = input_array_01
+
+    #Get the area of the input_array region of interest as number of pixels of interest
+    area_as_poi_number = np.sum(array_to_process)
+
+    #Use regionprops to calculate the area of the binary mask
+    array_to_process_props = regionprops(array_to_process)
+
+    #Get the area of the input_array region of interest number of pixels scaled by pixel-area
+    area_from_props = array_to_process.area
+
+    return area_as_poi_number, area_from_props
 
 
 def get_areas_of_regions_in_mask():
